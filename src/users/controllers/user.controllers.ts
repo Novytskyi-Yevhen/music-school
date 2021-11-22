97b8428cc8e043223e5dac5ea7dc14a1bacad341
Yevhen <serverdog1@gmail.com>
2021-11-22 15:03:56 +0200

    Update delete method on user.controllers

--- src/users/controllers/user.controllers.ts
+++ src/users/controllers/user.controllers.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { User } from 'src/entity';
 import { UsersDTO } from '../dto/usersDTO';
 import { UserService } from '../providers/user.service';
@@ -13,23 +22,25 @@ export class UserController {
   }
 
   @Patch('/update/:id')
-  async update(@Param('id') id: number, @Body() data: Partial<UsersDTO>){
+  async update(@Param('id') id: number, @Body() data: Partial<UsersDTO>) {
     await this.userService.update(id, data);
   }
 
   @Post('/create')
-  async createUser(@Body() data: UsersDTO){
-      return await this.userService.create(data);
+  async createUser(@Body() data: UsersDTO) {
+    return await this.userService.create(data);
   }
   @Delete('/delete/:id')
-  async delete(@Param('id') id: number){
-    let {affected} = await this.userService.delete(id);
-    return affected === 0 ? {
-      statusCode: HttpStatus.OK,
-      message: 'User deleted successfully',
-    } : {
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: 'User for delete is not found'
-    };
+  async delete(@Param('id') id: number) {
+    let { affected } = await this.userService.delete(id);
+    return affected === 0
+      ? {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: 'User for delete is not found',
+        }
+      : {
+          statusCode: HttpStatus.OK,
+          message: 'User deleted successfully',
+        };
   }
 }
